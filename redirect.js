b409049064e27427f2aa7e9fc13e17a862a3c12b
Rey <rey@live.jp>
2015-08-14 16:21:31 +0800

    Added href for CSC auto-approved link request form

--- redirect.js
+++ redirect.js
@@ -15,13 +15,11 @@ if (typeof $_GET['n'] === 'undefined' || $_GET['n'] == "") {
     $.ajax({
         url: 'https://docs.google.com/spreadsheets/u/2/d/1LXH1VCQ1n2e_A0-TDnQg9mt1s_PIi635wgv7vnulMe0/gviz/tq?tq=select+D+where+lower(E)%20=lower(%20%22'+$_GET['n']+'%22)+AND+F=%22Y%22+limit+1&tqx=responseHandler:loadDone;#',
         dataType: "jsonp"
-        
     });
 
     $.ajax({
         url: 'https://docs.google.com/spreadsheets/u/2/d/1xC5bHvCS8fGMJF2WLxAOXwi3dEPb74ZtViEHR_85Wnw/gviz/tq?tq=select+B+where+lower(C)%20=lower(%20%22'+$_GET['n']+'%22)+limit+1&tqx=responseHandler:loadDone;#',
         dataType: "jsonp"
-        
     });
 }
 function loadDone(data) {
